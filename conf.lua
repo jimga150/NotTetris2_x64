faec07fb60e2d1d5d8aeef5c6fe96caf81501f6b
Unknown <jimbosned@gmail.com>
2019-10-21 13:27:47 -0400

    Update author

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
 	t.title = "Not Tetris 2"
-	t.author = "Maurice"
+	t.author = "Maurice & jimga150"
 	t.identity = "not_tetris_2"
 	t.screen.width = 800
 	t.screen.height = 720
